2992e88db1c6b1003c03902399acd0e069785110
Michael M <makin99@yandex.ru>
2022-04-22 10:59:03 +0500

    added push notifications for android

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
+import OneSignal from 'react-native-onesignal';
 
 class App extends React.Component {
 
@@ -7,12 +8,55 @@ class App extends React.Component {
     super(props)
     this.webview = null
     this.onMessage = this.onMessage.bind(this)
+    this.loadOneSignal = this.loadOneSignal.bind(this)
+  }
+
+  loadOneSignal(info) {
+    OneSignal.setLogLevel(6, 0);
+    OneSignal.setAppId("559ca145-81b0-425f-b327-249c49c809d9");
+    
+    //Prompt for push on iOS
+    OneSignal.promptForPushNotificationsWithUserResponse(response => {
+      console.log("Prompt response:", response);
+    });
+    OneSignal.setExternalUserId(info)
+    //Method for handling notifications received while app in foreground
+    OneSignal.setNotificationWillShowInForegroundHandler(notificationReceivedEvent => {
+      console.log("OneSignal: notification will show in foreground:", notificationReceivedEvent);
+      let notification = notificationReceivedEvent.getNotification();
+      console.log("notification: ", notification);
+      const data = notification.additionalData
+      console.log("additionalData: ", data);
+      // Complete with null means don't show a notification.
+      notificationReceivedEvent.complete(notification);
+    });
+    
+    //Method for handling notifications opened
+    OneSignal.setNotificationOpenedHandler(notification => {
+      console.log("OneSignal: notification opened:", notification);
+      if (notification.notification.launchURL) {
+        const route = notification.notification.launchURL
+        console.log(`launchURL: ${route}`)
+        this.webview.postMessage(`push came route ${route}`)
+      }
+      
+    });
   }
 
   onMessage(event) {
-    // alert(`onMessage props: , ${event.nativeEvent.data}`)
-    const webDebug = !!event.nativeEvent.data
-    this.webview.postMessage(webDebug)
+    console.log(event.nativeEvent.data)
+    if (event.nativeEvent.data === 'MOBILEWEB_DEBUG false') {
+      this.webview.postMessage('MOBILEWEB_DEBUG true')
+      return
+    }
+    if (event.nativeEvent.data.includes('init push')) {
+      console.log('init push')
+      const phone = event.nativeEvent.data.replace('init push ', '')
+      console.log(phone)
+      this.loadOneSignal(phone)
+      return
+    }
+    
   }
 
   render() {
